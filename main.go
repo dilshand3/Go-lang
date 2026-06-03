cafa004c0ec0c9e70340a4a1c3726f01ee3a8489
dilshand3 <dilshan72404@gmail.com>
2026-06-03 22:56:13 +0530

    chore : add entry point of practice

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"go-notes/functions"
+)
+
+func main() {
+	// functions.GetSimpleMessage();
+	// functions.ReturnFun("dilshan");
+	// fmt.Println(functions.Sum(2, 2));
+	fmt.Println(functions.Multiply(3, 4))
+}
