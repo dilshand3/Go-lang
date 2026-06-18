81fa3ce3d4923f128defb557b58f834d196c83c3
dilshand3 <dilshan72404@gmail.com>
2026-06-18 22:50:10 +0530

    chore : add switch statement

---
+++ switchStatement/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	day := 3;
+
+	switch day {
+	case 1:
+		fmt.Println("this is 1")
+	case 2:
+		fmt.Println("this is 2")
+	case 3:
+		fmt.Println("this is 3")
+	default:
+		fmt.Println("this is default")
+	}
+}
