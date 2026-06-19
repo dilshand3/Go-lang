32b4d7de005f3a0d003e171fbd678e4829e29d9a
dilshand3 <dilshan72404@gmail.com>
2026-06-19 23:01:34 +0530

    chore : add extra function

--- array/main.go
+++ array/main.go
@@ -7,6 +7,15 @@ var arr = [3]int{1, 2, 3}
 func main() {
 	fruitArr := [3]string{"apple", "banana", "orange"}
 
-	fmt.Println("This is my fruit: ", fruitArr);
+	fmt.Println("This is my fruit: ", fruitArr)
 	fmt.Println(len(fruitArr))
+
+	myArray()
+
+}
+
+func myArray() {
+	sportsArr := [2]string{"cricket", "football"}
+
+	fmt.Println(sportsArr)
 }
