3a9c9842db8b6bcdc1f6fdff7840ebce166facae
dilshand3 <dilshan72404@gmail.com>
2026-06-19 22:47:55 +0530

    chore : add bufio for os package

--- userInput/main.go
+++ userInput/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main(){
-	fmt.Println("Enter user name");
+func main() {
+	fmt.Println("Enter user name")
 
-	var name string;
+	// var name string;
 
-	fmt.Scan(&name);
+	// fmt.Scan(&name);
 
-	fmt.Println("This is username:",name)
+	// fmt.Println("This is username:",name)
+
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\n')
+
+	fmt.Println(name)
 }
